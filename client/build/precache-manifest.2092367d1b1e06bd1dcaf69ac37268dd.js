7de9b60cf270754ff11918608d60eb99d8edb5bc
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2020-01-09 09:36:47 +0530

    Add files via upload

---
+++ client/build/precache-manifest.2092367d1b1e06bd1dcaf69ac37268dd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6a66b43caa79d8cfb9cbbe886a0d6d1",
+    "url": "/aahvaan_new/index.html"
+  },
+  {
+    "revision": "8ddf6bbe72aa39433682",
+    "url": "/aahvaan_new/static/css/main.eb131354.chunk.css"
+  },
+  {
+    "revision": "172cdd3fe6dff7cbe806",
+    "url": "/aahvaan_new/static/js/2.54441b39.chunk.js"
+  },
+  {
+    "revision": "e20aaf2b2e93ecd67e58d7b4121292f6",
+    "url": "/aahvaan_new/static/js/2.54441b39.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8ddf6bbe72aa39433682",
+    "url": "/aahvaan_new/static/js/main.f9124cc7.chunk.js"
+  },
+  {
+    "revision": "629b721d49fd7cf77aa1",
+    "url": "/aahvaan_new/static/js/runtime-main.ff963023.js"
+  },
+  {
+    "revision": "d96e65e9b9746e9fcadae7da998086f3",
+    "url": "/aahvaan_new/static/media/1.d96e65e9.png"
+  },
+  {
+    "revision": "bfb6ddb782c526471a80e37ca41d3535",
+    "url": "/aahvaan_new/static/media/2.bfb6ddb7.png"
+  },
+  {
+    "revision": "873df4971d5a438403270ca933947578",
+    "url": "/aahvaan_new/static/media/3.873df497.png"
+  },
+  {
+    "revision": "23a4c32d7cd263d63de0bfdcfb19595c",
+    "url": "/aahvaan_new/static/media/4.23a4c32d.png"
+  },
+  {
+    "revision": "da3f58f3ef3bdc53c49c2260275bac56",
+    "url": "/aahvaan_new/static/media/5.da3f58f3.png"
+  },
+  {
+    "revision": "50045998ba1507ba948653738714d9c4",
+    "url": "/aahvaan_new/static/media/6.50045998.png"
+  },
+  {
+    "revision": "e46a9455283c99068e3b2da7ccabf98a",
+    "url": "/aahvaan_new/static/media/7.e46a9455.png"
+  },
+  {
+    "revision": "de8a56e3016f50d3c37daebce07c42ac",
+    "url": "/aahvaan_new/static/media/logo.de8a56e3.png"
+  }
+]);
